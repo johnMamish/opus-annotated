ffbe9e3cc26b68be1629cf0ed7029d0064a92fd0
John Mamish <john.mamish@gmail.com>
2021-06-21 18:22:41 -0500

    annotation of laplace encode basically done

--- celt/laplace.c
+++ celt/laplace.c
@@ -70,7 +70,35 @@
  *
  * Once the distribution's probability falls below a certain value (LAPLACE_MINP), the remainder
  * of the distribution consists LAPLACE_MINP until the cumulative value of the distribution reaches
- * '1'.
+ * '1'. After that point, the probability of higher values is 0.
+ *
+ * The only module that uses this to encode symbols is the CELT coarse energy encoder. As a result,
+ * we can have an upper bound on how many values we need above or below zero. This is defined by
+ * LAPLACE_NMIN. If LAPLACE_NMIN is 16, The resulting laplace distribution is guaranteed to encode
+ * values in [-16, 16]. Once the value goes above 16, the probability of encoding that symbol might
+ * be 0. There should be at least 3 or 4 values before the curve hits p_min, so you should be able
+ * to encode values on [-v, v] for v > 20. If you try to encode a value that's so large it "walks
+ * off" the end of the curve, the encoder clamps your value to the maximum one allowed.
+ *
+ * For bands whose value is really expected to be close to 0 (which should be a typical case),
+ * having a very aggressive (small) decay value will make the PDF largely clumped around 0,
+ * decreasing the cost of encoding a 0. For bands whose value is unpredictable (like the first
+ * band in an intra frame), the probability of a 0 is relatively small and the decay is large,
+ * meaning that more bits need to be used to encode values because there will be more variance
+ * in the distribution hence the range encoder will have more entropy to encode.
+ *
+ * Negative values
+ * Because all range coder symbols are positive, we encode negative values by "folding over" the
+ * negative half of the spectrum and interleaving negative values with positive ones. For instance,
+ *     symbol value       coded value
+ *     0                   0
+ *     1                  -1
+ *     2                   1
+ *     3                  -2
+ *     4                   2
+ *    ...
+ *     n                   floor((n - 1) / 2) for even n
+ *                        -floor((n - 1) / 2) for odd n
  */
 
 /**
@@ -124,12 +152,6 @@ void ec_laplace_encode(ec_enc *enc, int *value, unsigned fs, int decay)
    //   *
    if (val)
    {
-      int s = 0;
-      if (val < 0) {
-          s = -1;
-          val = -val;
-      }
-
       // If the value is 0,
       int i;
 
@@ -139,7 +161,7 @@ void ec_laplace_encode(ec_enc *enc, int *value, unsigned fs, int decay)
 
       /* Search the decaying part of the PDF.*/
       // scoot fl forward until until we find the value we want or we hit the end of the curve.
-      for (i = 1; (fs >= LAPLACE_MINP) && (i < val); i++)
+      for (i = 1; (fs >= LAPLACE_MINP) && (i < abs(val)); i++)
       {
          // we need to multiply fs by 2 because we have to consider both positive and negative
          // sides of the curve (right?)
@@ -154,31 +176,56 @@ void ec_laplace_encode(ec_enc *enc, int *value, unsigned fs, int decay)
       if (fs < LAPLACE_MINP)
       {
          // val is in a bucket with probability LAPLACE_MINP
-         // ndi_max is number of buckets over which the remaining probability is divided.
+         // ndi_max is number of buckets over which the remaining probability is divided once
+         // fs reaches a point where its probability is LAPLACE_MINP.
          int ndi_max;
          ndi_max = (32768 - fl + LAPLACE_MINP - 1) >> LAPLACE_LOG_MINP;
-         ndi_max = (ndi_max - s) >> 1;
 
-         // If
-         int di = IMIN(val - i, ndi_max - 1);
+         if (val < 0) {
+             ndi_max = (ndi_max + 1) >> 1;
+         } else {
+             ndi_max >>= 1;
+         }
 
-         //
-         fl += (2*di+1+s)*LAPLACE_MINP;
+
+         // If the value we wanted to encode is off of the tail end of the distribution. Round it down
+         // int di = IMIN(val - i, ndi_max - 1);
+         // di contains the number of LAPLACE_MINP buckets that we need to scroll forward by
+         int di;
+         if ((ndi_max - 1) < (abs(val) - i)) {
+             printf("Warning: we walked off of the end of the laplace distribution during encoding.\n");
+             di = ndi_max - 1;
+         } else {
+             di = abs(val) - i;
+         }
+
+         if (val < 0) {
+             fl += (2 * di) * LAPLACE_MINP;
+             *value = -(i * di);
+         } else {
+             fl += (2 * di + 1) * LAPLACE_MINP;
+             *value = i * di;
+         }
+
          fs = IMIN(LAPLACE_MINP, 32768-fl);
-         *value = (i+di+s)^s;
       }
       else
       {
          // val is not in a bucket w probability LAPLACE_MINP
          // nudge fs up by
          fs += LAPLACE_MINP;
-         fl += fs&~s;
+
+         // if val is positive, scoot fl up by 1x fs
+         if (val > 0) {
+             fl += fs;
+         }
       }
-      celt_assert(fl+fs<=32768);
-      celt_assert(fs>0);
+
+      celt_assert((fl + fs) <= 32768);
+      celt_assert(fs > 0);
    }
 
-   printf("encoding value %i with (fl, fs) = (%i, %i)\n", *value, fl, fs);
+   printf("encoding value %i with (fl, fs) = (%i, %i); fl + fs = %i\n", *value, fl, fs, fl + fs);
    ec_encode_bin(enc, fl, fl+fs, 15);
 }
 
